210a6ac8ba6e8bee9fedf068c00531a90d02d3ca
Irvan <hardyanto.irvan0398@gmail.com>
2019-10-22 15:32:29 +0700

    Tambah import yang dibutuhkan dan contoh penggunaan

--- MyUtils.java
+++ MyUtils.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException; 
+
 class MyUtils{
 	//references: https://stackoverflow.com/questions/1844688/how-to-read-all-files-in-a-folder-from-java
+	
+	//CONTOH PENGUNAAN: Bila folder dataset mentah terdapat pada direktori C:/xampp/htdocs/TugasPTKI/raw_dataset,
+	//maka untuk mendapatkan seluruh file caranya:
+	//File input = new File("C:/xampp/htdocs/TugasPTKI/raw_dataset")
+	//ArrayList<File> files = MyUtils.listFilesForFolder(input)
+	
+	/**
+	* Membaca seluruh File pada sebuah folder
+	* @param folder Objek File yang menyatakan direktori folder berisi file-file yang ingin dibaca
+	* @return ArrayList Objek File
+	**/
 	public static ArrayList<File> listFilesForFolder(File folder){
 		ArrayList<File> files = new ArrayList<File>();
 		//menggunakan listFiles() biar langsung dapet objek File nya,
@@ -29,6 +48,11 @@ class MyUtils{
         }
 	}
 	
+	/**
+	* Mengembalkan sebuah ArrayList yang berisi tiap baris pada dokumen
+	* @param f File yang ingin dibaca
+	* @return ArrayList yang bersi tiap baris pada dokumen
+	**/
 	public static ArrayList<String> readAll(File f){
             ArrayList<String> lines = new ArrayList<String>();
             try{
