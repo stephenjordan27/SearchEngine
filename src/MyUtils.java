48bb179626018f600a4e107753c9951b020a17be
IrvanHardyanto98 <hardyanto.irvan0398@gmail.com>
2019-10-23 19:33:29 +0700

    Tambah Stop words dan Stemming, revisi MyUtils

--- src/MyUtils.java
+++ src/MyUtils.java
@@ -1,72 +1,151 @@
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.IOException; 
+import java.io.IOException;
 
-class MyUtils{
-	//references: https://stackoverflow.com/questions/1844688/how-to-read-all-files-in-a-folder-from-java
-	
-	//CONTOH PENGUNAAN: Bila folder dataset mentah terdapat pada direktori C:/xampp/htdocs/TugasPTKI/raw_dataset,
-	//maka untuk mendapatkan seluruh file caranya:
-	//File input = new File("C:/xampp/htdocs/TugasPTKI/raw_dataset")
-	//ArrayList<File> files = MyUtils.listFilesForFolder(input)
-	
-	/**
-	* Membaca seluruh File pada sebuah folder
-	* @param folder Objek File yang menyatakan direktori folder berisi file-file yang ingin dibaca
-	* @return ArrayList Objek File
-	**/
-	public static ArrayList<File> listFilesForFolder(File folder){
-		ArrayList<File> files = new ArrayList<File>();
-		//menggunakan listFiles() biar langsung dapet objek File nya,
-		//kalau list() hanya string nama file nya saja.
-		for(File f : folder.listFiles()){
-			if(f.isDirectory()){
-				listFilesForFolder(f);
-			}else{
-				files.add(f);
-				//System.out.println(f.getName());
-			}
-		}
-		return files;
-	}
-	
-	public static void readAllVoid(File f){
-        try{
-            BufferedReader br = new BufferedReader(new FileReader(f));
-			String line="";
-			while((line = br.readLine())!= null ){
-				System.out.println(line);
-			}
-        }catch(FileNotFoundException e){
-            System.out.println("Error: File: "+f.getName()+" not found!");
-        }catch(IOException e){
-            System.out.println("Error reading file : "+f.getName());
+import org.json.*;
+
+class MyUtils {
+    //references: https://stackoverflow.com/questions/1844688/how-to-read-all-files-in-a-folder-from-java
+
+    //CONTOH PENGUNAAN: Bila folder dataset mentah terdapat pada direktori C:/xampp/htdocs/TugasPTKI/raw_dataset,
+    //maka untuk mendapatkan seluruh file caranya:
+    //File input = new File("C:/xampp/htdocs/TugasPTKI/raw_dataset")
+    //ArrayList<File> files = MyUtils.listFilesForFolder(input)
+    /**
+     * Membaca seluruh File pada sebuah folder
+     *
+     * @param folder Objek File yang menyatakan direktori folder berisi
+     * file-file yang ingin dibaca
+     * @return ArrayList Objek File
+     *
+     */
+    public static ArrayList<File> listFilesForFolder(File folder) {
+        ArrayList<File> files = new ArrayList<File>();
+        //menggunakan listFiles() biar langsung dapet objek File nya,
+        //kalau list() hanya string nama file nya saja.
+        for (File f : folder.listFiles()) {
+            if (f.isDirectory()) {
+                listFilesForFolder(f);
+            } else {
+                files.add(f);
+                //System.out.println(f.getName());
+            }
         }
-	}
-	
-	/**
-	* Mengembalkan sebuah ArrayList yang berisi tiap baris pada dokumen
-	* @param f File yang ingin dibaca
-	* @return ArrayList yang bersi tiap baris pada dokumen
-	**/
-	public static ArrayList<String> readAll(File f){
-            ArrayList<String> lines = new ArrayList<String>();
-            try{
-                BufferedReader br = new BufferedReader(new FileReader(f));
-		String line = "";
-		
-		while((line = br.readLine())!= null ){
-			lines.add(line);
-		}
-            }catch(FileNotFoundException e){
-                System.out.println("Error: File: "+f.getName()+" not found!");
-            }catch(IOException e){
-                System.out.println("Error reading file : "+f.getName());
-            }	
-            return lines;
-	}
+        return files;
+    }
+
+    /**
+     * Code By Stephen Jordan 2016730018
+     *
+     */
+    public static String[] readFiles(String dir) {
+        String[] output = new String[154];
+        String input = "";
+        try {
+            for (int i = 1; i <= 154; i++) {
+                String name = "";
+                if (i < 10) {
+                    name = "00" + i;
+                } else if (i >= 10 && i < 100) {
+                    name = "0" + i;
+                } else if (i >= 100 && i <= 154) {
+                    name = "" + i;
+                }
+
+                // initialize input
+                BufferedReader br = new BufferedReader(new FileReader(dir + "/Doc" + name + ".txt"));
+                StringBuilder sb = new StringBuilder();
+                String line = br.readLine();
+                while (line != null) {
+                    sb.append(line);
+                    sb.append(System.lineSeparator());
+                    line = br.readLine();
+                }
+                input = sb.toString();
+
+                br.close();
+
+                output[i - 1] = input;
+                input = "";
+            }
+        } catch (IOException e) {
+
+        }
+        return output;
+    }
+
+    public static void readAllVoid(File f) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File: " + f.getName() + " not found!");
+        } catch (IOException e) {
+            System.out.println("Error reading file : " + f.getName());
+        }
+    }
+
+    /**
+     * Mengembalkan sebuah ArrayList yang berisi tiap baris pada dokumen
+     *
+     * @param f File yang ingin dibaca
+     * @return ArrayList yang bersi tiap baris pada dokumen
+     *
+     */
+    public static ArrayList<String> readAll(File f) {
+        ArrayList<String> lines = new ArrayList<String>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            String line = "";
+
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File: " + f.getName() + " not found!");
+        } catch (IOException e) {
+            System.out.println("Error reading file : " + f.getName());
+        }
+        return lines;
+    }
+
+    public static String statistics(File inputDir, String unit) {
+        ArrayList<File> files = listFilesForFolder(inputDir);
+        Iterator<File> iFile = files.iterator();
+
+        long total_words = 0;
+        long per_doc_words = 0;
+        long doc_num = files.size();
+
+        JSONObject resObj = new JSONObject();
+        try {
+            resObj.put("doc_num", doc_num);
+
+            while (iFile.hasNext()) {
+                ArrayList<String> lines = readAll(iFile.next());
+                Iterator<String> iString = lines.iterator();
+                while (iString.hasNext()) {
+                    total_words += iString.next().split(" ").length;
+                }
+            }
+
+            resObj.put("total_" + unit + "s", total_words);
+
+            double avg_word = total_words / doc_num * 1.0;
+
+            resObj.put("avg_" + unit + "s", avg_word);
+        } catch (JSONException e) {
+            System.out.println("JSON exception");
+        }
+
+        return resObj.toString();
+    }
 }
--- src/Tester.java
+++ src/Tester.java
@@ -1,8 +1,10 @@
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -17,105 +19,79 @@ import lib.stopwords.WordIterator;
  * To change this inputlate file, choose Tools | Templates
  * and open the inputlate in the editor.
  */
-
 /**
  *
  * @author asus
  */
-
-
 public class Tester {
+
     public static void main(String[] args) throws FileNotFoundException, IOException {
         String input = "";
-        String[] outputTXT = new String[154];
-        String[] outputStopwords = new String[154];
-        
-        // TXT File
-        for (int i = 1; i <= 154; i++) {
-            String name = "";
-            if(i<10){
-                name = "00"+i;
-            }
-            else if(i>=10 && i<100){
-                name = "0"+i;
-            }
-            else if(i>=100 && i<=154){
-                name = ""+i;
-            }
-            
-            // initialize input
-            BufferedReader br = new BufferedReader(new FileReader("C:/Users/asus/Desktop/DataSet/Doc"+name+".txt"));
-            try {
-                StringBuilder sb = new StringBuilder();
-                String line = br.readLine();
-                while (line != null) {
-                    sb.append(line);
-                    sb.append(System.lineSeparator());
-                    line = br.readLine();
-                }
-                input = sb.toString();
-            } 
-            finally {
-                br.close();
-            }
-            outputTXT[i-1]=input;
-            input = "";
-        }
-        
-        // Stopwords
-        for (int i = 1; i <= 154; i++) {
-            for (final String word : new WordIterator(outputTXT[i-1])) {
-                if (StopWords.English.isStopWord(word) == false) {
-                    input += word+" ";
-                }
-            }
-            outputStopwords[i-1]=input;
-            input = "";
-        }
-        
-        // Porter Stemmer
-        for (int i = 1; i <= 154; i++) {
-            porterStemmer stemmer = new porterStemmer(); 
-            input = outputStopwords[i-1];
-            String[] arrInput =  input.split(" ");
-            for (int j = 0; j < arrInput.length; j++) {
-                stemmer.setCurrent(arrInput[j]);
-                stemmer.stem();
-                input += stemmer.getCurrent()+" ";
-            }
-            
-            //print output
-            System.out.println(i+". "+input);
-            input = "";
-        }
-        
-        System.out.println("");
-        
-        //Statistik : Jumlah dokumen
-        System.out.println("Jumlah dokumen");
-        File inputFile = new File("C://Users//asus//Desktop//DataSet");
-	ArrayList<File> files = MyUtils.listFilesForFolder(inputFile);
-        System.out.println(files.size());
-        System.out.println("");
-        
-        //Statistik : Jumlah words dari seluruh dokumen
-        System.out.println("Jumlah words dari seluruh dokumen");
-        int jumlahWords = 0;
-        for (int i = 1; i <= 154; i++) {
-            input = outputTXT[i-1];
-            String[] words = input.replaceAll("\r\n", " ").replaceAll("[^a-zA-Z] ", " ").split(" ");
-            jumlahWords+=words.length;
-        }
-         System.out.println(jumlahWords);
-        
-        System.out.println("");
-        
-        //Statistik : Jumlah rata-rata words per dokumen
+        String[] outputTXT;
+        String[] outputStopwords;
+        String[] outputStemmer;
 
-        System.out.println("Jumlah rata-rata words per dokumen");
-        int rata2 = jumlahWords/154;
-        System.out.println(rata2);
-         
-        
+        // TXT File
+//        outputTXT = MyUtils.readFiles("F:\\SEMESTER 7 GANJIL 2019-2020\\Pencarian dan Temu Kembali Informasi\\YouRSearchEngine\\raw_dataset");
+//        outputStopwords = Preprocessor.removeStopWords(outputTXT);
+//        outputStemmer = Preprocessor.stem(outputStopwords);
+//        for (int i = 0; i < outputStemmer.length; i++) {
+//            System.out.println((i+1)+". "+outputStemmer[i]);
+//        }
+
+
+File inputDir = new File("F:/SEMESTER 7 GANJIL 2019-2020/Pencarian dan Temu Kembali Informasi/YouRSearchEngine/raw_dataset");
+		File outputDir = new File("F:/SEMESTER 7 GANJIL 2019-2020/Pencarian dan Temu Kembali Informasi/YouRSearchEngine/preprocessed_dataset");
+		
+		Preprocessor p =new Preprocessor();
+		long startTime = System.currentTimeMillis();
+			
+		p.preProcess(inputDir,outputDir);
+			
+		long endTime = System.currentTimeMillis();
+		
+		System.out.println();
+		System.out.println("Time ellapsed: "+(endTime-startTime));
+		
+		File statsDir= new File("F:/SEMESTER 7 GANJIL 2019-2020/Pencarian dan Temu Kembali Informasi/YouRSearchEngine/stat.json");
+		try{
+			BufferedWriter bw  = new BufferedWriter(new FileWriter(statsDir));
+			bw.write(MyUtils.statistics(inputDir,"word"));
+			bw.write(MyUtils.statistics(outputDir,"term"));
+			bw.close();
+		}catch(IOException e){
+			
+		}
+        // Stopwords
+       
+
+        // Porter Stemmer
+
+//        System.out.println("");
+//
+//        //Statistik : Jumlah dokumen
+//        System.out.println("Jumlah dokumen");
+//        File inputFile = new File("C://Users//asus//Desktop//DataSet");
+//        ArrayList<File> files = MyUtils.listFilesForFolder(inputFile);
+//        System.out.println(files.size());
+//        System.out.println("");
+//
+//        //Statistik : Jumlah words dari seluruh dokumen
+//        System.out.println("Jumlah words dari seluruh dokumen");
+//        int jumlahWords = 0;
+//        for (int i = 1; i <= 154; i++) {
+//            input = outputTXT[i - 1];
+//            String[] words = input.replaceAll("\r\n", " ").replaceAll("[^a-zA-Z] ", " ").split(" ");
+//            jumlahWords += words.length;
+//        }
+//        System.out.println(jumlahWords);
+//
+//        System.out.println("");
+//
+//        //Statistik : Jumlah rata-rata words per dokumen
+//        System.out.println("Jumlah rata-rata words per dokumen");
+//        int rata2 = jumlahWords / 154;
+//        System.out.println(rata2);
+
     }
 }
